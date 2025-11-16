35c6d82c7e496bced85ecba51ccef14db32bcfe5
Sprajapati123 <sandisprajapati52@gmail.com>
2025-11-16 15:15:46 +0545

    changes

--- app/src/main/java/com/example/ai37b/ui/theme/Color.kt
+++ app/src/main/java/com/example/ai37b/ui/theme/Color.kt
@@ -3,7 +3,10 @@ package com.example.ai37b.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+val Blue = Color(0xFF5D78BC)
+val PurpleGrey80 = Color(0xFFE9E6EE)
 val Pink80 = Color(0xFFEFB8C8)
 
 val Purple40 = Color(0xFF6650a4)
