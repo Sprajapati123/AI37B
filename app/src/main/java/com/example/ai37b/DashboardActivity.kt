597de26b4e66787b45e3845891c2cb414b053505
Sprajapati123 <sandisprajapati52@gmail.com>
2025-11-30 14:29:48 +0545

    appbar

--- app/src/main/java/com/example/ai37b/DashboardActivity.kt
+++ app/src/main/java/com/example/ai37b/DashboardActivity.kt
@@ -8,14 +8,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.ai37b.ui.theme.AI37BTheme
+import com.example.ai37b.ui.theme.Blue
+import com.example.ai37b.ui.theme.White
 
 class DashboardActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,6 +36,7 @@ class DashboardActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DashboardBody() {
 
@@ -36,7 +46,41 @@ fun DashboardBody() {
     val email = activity.intent.getStringExtra("email")
     val password = activity.intent.getStringExtra("password")
 
-    Scaffold { padding ->
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Blue,
+                    titleContentColor = Color.White,
+                    navigationIconContentColor = White,
+                    actionIconContentColor = White
+                ),
+                title = { Text("Ecommerce") },
+                navigationIcon = {
+                    IconButton(onClick = {
+
+                        activity.finish()
+                    }) {
+                        Icon(
+                            painter = painterResource(R.drawable.baseline_arrow_back_ios_24),
+                            contentDescription = null
+                        )
+
+                    }
+                },
+                actions = {
+                    IconButton(onClick = {
+                    }){
+                        Icon(painter = painterResource(R.drawable.baseline_visibility_24),contentDescription = null)
+                    }
+                    IconButton(onClick = {
+                    }){
+                        Icon(painter = painterResource(R.drawable.baseline_visibility_24),contentDescription = null)
+                    }
+                }
+            )
+        }
+    ) { padding ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
