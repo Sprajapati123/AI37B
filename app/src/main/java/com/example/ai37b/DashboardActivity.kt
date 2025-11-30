18465fb70302ae2ba1d595bffbc7cf4f97482e95
Sprajapati123 <sandisprajapati52@gmail.com>
2025-11-30 15:32:07 +0545

    changes

--- app/src/main/java/com/example/ai37b/DashboardActivity.kt
+++ app/src/main/java/com/example/ai37b/DashboardActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -12,11 +13,17 @@ import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -46,6 +53,21 @@ fun DashboardBody() {
     val email = activity.intent.getStringExtra("email")
     val password = activity.intent.getStringExtra("password")
 
+    data class NavItem(val label: String, val icon: Int)
+
+    var selectedItem by remember { mutableStateOf(0) }
+
+    var navList = listOf(
+        NavItem("Home", R.drawable.baseline_home_24),
+        NavItem("Search", R.drawable.baseline_search_24),
+        NavItem("Notification", R.drawable.baseline_notifications_24),
+        NavItem("Profile", R.drawable.baseline_person_24),
+    )
+
+//    for(int i = 0;i<navList.size;i++){
+//        navlist[i].label
+//        navList[i].icon
+//    }
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
@@ -70,25 +92,52 @@ fun DashboardBody() {
                 },
                 actions = {
                     IconButton(onClick = {
-                    }){
-                        Icon(painter = painterResource(R.drawable.baseline_visibility_24),contentDescription = null)
+                    }) {
+                        Icon(
+                            painter = painterResource(R.drawable.baseline_visibility_24),
+                            contentDescription = null
+                        )
                     }
                     IconButton(onClick = {
-                    }){
-                        Icon(painter = painterResource(R.drawable.baseline_visibility_24),contentDescription = null)
+                    }) {
+                        Icon(
+                            painter = painterResource(R.drawable.baseline_visibility_24),
+                            contentDescription = null
+                        )
                     }
                 }
             )
+        },
+        bottomBar = {
+            NavigationBar {
+                navList.forEachIndexed { index, item ->
+                    NavigationBarItem(
+                        icon = {
+                            Icon(painter = painterResource(item.icon),
+                                contentDescription = null)
+                        },
+                        label = {Text(item.label)},
+                        onClick = {
+                            selectedItem = index
+                        },
+                        selected = selectedItem == index
+                    )
+                }
+            }
         }
     ) { padding ->
-        Column(
+        Box(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(padding)
         ) {
-            Text("Email: $email")
-            Text("Password: $password")
-
+            when(selectedItem){
+                0 -> HomeScreen()
+                1 -> SearchScreen()
+                2 -> NotificationScreen()
+                3 -> ProfileScreen()
+                else -> HomeScreen()
+            }
         }
     }
 }
---
+++ app/src/main/java/com/example/ai37b/UserModel.kt
@@ -0,0 +1,4 @@
+package com.example.ai37b
+
+//data class UserModel()
+
